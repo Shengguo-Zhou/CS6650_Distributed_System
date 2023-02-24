1241e79a3ca0473a56d369437efb83c4870acc56
Shengguo Zhou <96205839+Shengguo-Zhou@users.noreply.github.com>
2023-02-23 23:02:43 -0800

    a2

---
+++ assignment2/client/src/main/java/MultiThread.java
@@ -0,0 +1,83 @@
+import event.GraphPlot;
+import event.OutputCSV;
+import event.ResultList;
+import event.ResultProcess;
+import event.SwipeEvent;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultiThread {
+  private static String IP;
+  private static BlockingQueue<SwipeEvent> queue;
+  private static AtomicInteger winCount;
+  private static AtomicInteger loseCount;
+  private static Integer numThreads = 100;
+  private static Integer totalCount = 500_000;
+
+  public static void main(String[] args) throws InterruptedException {
+    System.out.println("Start from here");
+
+    IP = "http://localhost:8080/assignment1_war_exploded/skiers/";
+//    IP = "http://52.25.155.80:8080/assignment1_war/skiers/";
+    queue = new LinkedBlockingQueue<>();
+    winCount = new AtomicInteger(0);
+    loseCount = new AtomicInteger(0);
+    CountDownLatch latch1 = new CountDownLatch(numThreads);
+    long start = System.currentTimeMillis();
+    BodyList bodyList = new BodyList(queue, totalCount);
+    bodyList.generateList();
+    ResultList resultList = new ResultList();
+
+    for (int i = 0; i < numThreads; i++) {
+      RunInOneThread r1 = new RunInOneThread(IP, queue, winCount, loseCount, latch1, resultList);
+      Thread thread1 = new Thread(r1);
+      thread1.start();
+      System.out.println(i);
+    }
+
+    latch1.await();
+    long end = System.currentTimeMillis();
+    long lastTime = end - start;
+    OutputCSV outputCSV = new OutputCSV();
+    try {
+      outputCSV.outPutCSVToPath(resultList.getResultList(),
+          "/Users/shengguozhou/Desktop/6650distributed_system/");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    GraphPlot graphPlot = new GraphPlot(resultList);
+    List<Integer> timeCount = graphPlot.getList();
+    try {
+      outputCSV.outPutCSVToPath2(timeCount,
+          "/Users/shengguozhou/Desktop/6650distributed_system/");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    ResultProcess resultProcess = new ResultProcess(resultList.getResultList());
+    double mean = resultProcess.mean();
+    double median = resultProcess.median();
+    double p99 = resultProcess.get99p();
+    double max = resultProcess.max();
+    double min = resultProcess.min();
+
+    System.out.println("This is the End");
+    System.out.println("Number of successful requests:" + winCount.get());
+    System.out.println("Number of failed requests:" + loseCount.get());
+    System.out.println("Total lasting time: " + lastTime);
+    System.out.println( "Throughput: " + (int)((winCount.get() + loseCount.get()) /
+        (double)(lastTime / 1000) )+ " requests/second");
+    System.out.println("Mean time: " + mean);
+    System.out.println("Median response time: " + median);
+    System.out.println("99th percentile response time: " + p99);
+    System.out.println("Max response time: " + max);
+    System.out.println("Min response time: " + min);
+  }
+
+}
---
+++ assignment2/server/src/main/java/SkierServlet.java
@@ -0,0 +1,118 @@
+import Entites.SwiperDetails;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.BufferedReader;
+import java.util.concurrent.TimeoutException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import org.apache.commons.pool.ObjectPool;
+import org.apache.commons.pool.impl.GenericObjectPool;
+
+@WebServlet(name = "SkierServlet", value = "/SkierServlet")
+public class SkierServlet extends HttpServlet {
+
+  @Override
+  public void init() throws ServletException {
+    Gson gson = new Gson();
+    ObjectPool<Channel> pool;
+    final String QUEUE = "ResultMQ";
+    ConnectionFactory connectionFactory = new ConnectionFactory();
+    connectionFactory.setHost("localhost");
+    connectionFactory.setUsername("admin");
+    connectionFactory.setPassword("admin");
+    Connection connection;
+    try {
+      connection = connectionFactory.newConnection();
+      pool = new GenericObjectPool<T>(connection.createChannel());
+    } catch (IOException | TimeoutException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/html");
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.getWriter().write("Assignment 1 doGet is working");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("application/json");
+    String urlPath = request.getPathInfo();
+
+    // url null check
+    if (urlPath == null || urlPath.isEmpty()) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      response.getWriter().write("URL Path is null, missing parameters");
+      return;
+    }
+
+    String[] urlParts = urlPath.split("/");
+
+    if (!isUrlValid(urlParts, response)) {
+//      response.getWriter().write("failed in valid check");
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } else {
+      //json;
+      processRequest(request, response);
+      response.getWriter().write("Assignment 1 doPost is working");
+      response.setStatus(HttpServletResponse.SC_CREATED);
+    }
+  }
+
+  private boolean isUrlValid(String[] urlPath, HttpServletResponse response) throws IOException {
+    // urlPath  = "/1/seasons/2019/day/1/skier/123"
+    // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
+//    if(urlPath.length < 6) {
+//      response.getWriter().write("too short");
+//      return false;
+//    }
+
+    if(!urlPath[1].equalsIgnoreCase("swipe")) {
+      response.getWriter().write("First String is not swipe");
+      response.getWriter().write(urlPath[1]);
+      return false;
+    }
+    if(!urlPath[2].equalsIgnoreCase("left") &&
+        !urlPath[2].equalsIgnoreCase("right")) {
+      response.getWriter().write("Second string is not left or right");
+      return false;
+    }
+
+    return true;
+  }
+
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+    ObjectMapper objectMapper = new ObjectMapper();
+    //try catch
+    StringBuilder sb = new StringBuilder();
+    BufferedReader bufferedReader = request.getReader();
+    while (bufferedReader.ready()){
+      sb.append(bufferedReader.readLine());
+    }
+
+    try{
+      SwiperDetails swiperDetails = objectMapper.readValue(sb.toString(), SwiperDetails.class);
+//      response.getOutputStream().print(gson.toJson(swiperDetails));
+      response.getWriter().print(gson.toJson(swiperDetails));
+      response.setStatus(HttpServletResponse.SC_CREATED);
+    } catch (Exception e){
+      response.getWriter().write("Handle json file error");
+    }
+
+  }
+
+
+}
+
